73b5d18160b002f8c61b13dda1e4e75c56dfbb9c
JoNoC0 <jnguyeningco@gmail.com>
2021-11-20 11:20:37 -0600

    pass and fail test employee

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,14 @@
+
+const Employee = require('./lib/Employee')
+
+describe('Employee', () => {
+    it('creates employee object', () => {
+        const employee = new Employee('John', 1, 'JohnDoe@email.com') 
+        
+
+        expect(employee.name).toBe('John')
+        expect(employee.id).toBe(1)
+        expect(employee.email).toBe('JohnDoe@email.com')
+    })
+}) 
+
