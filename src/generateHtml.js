ac9f5af0ad2c06c8d3037a39e875315db847080d
JoNoC0 <jnguyeningco@gmail.com>
2022-01-16 00:05:52 -0600

    generate employee cards

--- src/generateHtml.js
+++ src/generateHtml.js
@@ -1,32 +1,41 @@
-function generateHtml(employeeArr) {
-    let employeeCards = ''
-    // create employee cards and add to variable employeeCards using for loop to loop through array, if statements for what type of employee making
-    const log = employeeArr.map(function() {
-        console.log(employeeArr);
-    }) 
-
-    let main = `
+// generate html page
+const generateTeamPage = function (employeeCards) {
+    return`
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Team Profile</title>
     // use bootstrap to make cards add to employeeCards
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Team Profile</title>
+    // font-awesome
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <nav class ="navbar navbar-dark bg-dark mb-5">
-        <span class="navbar-brand mb-0 h1 w-100 text-center">Team Profile</span>
-    </nav>
-    <div class="container">
-        <div class="row>
-    ${employeeCards}
+    <header>
+        <nav class ="navbar navbar-dark bg-dark mb-5">
+            <span class="navbar-brand mb-0 h1 w-100 text-center">Team Profile</span>
+        </nav>
+    </header>
+    <main>
+        <div class="container">
+            <div class"row justify-content-center" id="team-cards">
+                <!--Team Cards-->
+                ${employeeCards}
+            </div>
+        </div>
+    </main>    
 </body>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </html>
-`
-    return main
+`;
 }
 
 module.exports = generateHtml;
